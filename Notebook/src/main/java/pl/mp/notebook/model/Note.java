32d16d7b5d9c67b0caddb95ca9c433090367bc17
Marta <martycjas13@gmail.com>
2019-02-14 16:25:15 +0100

    Note class

---
+++ Notebook/src/main/java/pl/mp/notebook/model/Note.java
@@ -0,0 +1,69 @@
+package pl.mp.notebook.model;
+
+import java.util.Date;
+
+
+public class Note {
+
+    private String title;
+
+    private String author;
+
+    private String text;
+
+    private Date date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Note note = (Note) o;
+
+        if (!title.equals(note.title)) return false;
+        if (!author.equals(note.author)) return false;
+        if (!text.equals(note.text)) return false;
+        return date.equals(note.date);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + text.hashCode();
+        result = 31 * result + date.hashCode();
+        return result;
+    }
+}
