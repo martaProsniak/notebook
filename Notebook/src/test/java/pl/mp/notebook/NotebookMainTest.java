c03ef4b29c3abc4e686055bf325e2b1abca72ac5
martaprosniak <martaprosniak@gmail.com>
2019-02-26 20:27:18 +0100

    NotebookMain test class

---
+++ Notebook/src/test/java/pl/mp/notebook/NotebookMainTest.java
@@ -0,0 +1,12 @@
+package pl.mp.notebook;
+
+import org.assertj.core.api.Assertions;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NotebookMainTest {
+
+
+}
